01bf4d4cffe6194ea79e55e6bed4cea6a2b7f9c2
Challelign Tilahun <chalie2123@gmail.com>
2023-09-26 10:00:08 +0300

    null map fix

--- src/components/WatchedSummary.jsx
+++ src/components/WatchedSummary.jsx
@@ -4,9 +4,9 @@ const WatchedSummary = ({ watched }) => {
 	const average = (arr) =>
 		arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-	const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-	const avgUserRating = average(watched.map((movie) => movie.userRating));
-	const avgRuntime = average(watched.map((movie) => movie.runtime));
+	const avgImdbRating = average(watched?.map((movie) => movie.imdbRating));
+	const avgUserRating = average(watched?.map((movie) => movie.userRating));
+	const avgRuntime = average(watched?.map((movie) => movie.runtime));
 	return (
 		<div className="summary">
 			<h2>Movies you watched</h2>
